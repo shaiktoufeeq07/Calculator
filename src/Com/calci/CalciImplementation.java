0b2e9f9e14601a8a061a2cdb8f990399e9c0a86f
aadityasankalp2125-beep <aadityasankalp.2125@gmail.com>
2025-11-18 18:24:48 +0530

    ADDED IMP LAYER

---
+++ src/Com/calci/CalciImplementation.java
@@ -0,0 +1,29 @@
+package Com.calci;
+
+public class CalciImplementation implements Calci{
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double div(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
