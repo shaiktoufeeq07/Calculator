29e51b1de5d6087b794e37d74bee819ae0f4b036
aadityasankalp2125-beep <aadityasankalp.2125@gmail.com>
2025-11-18 18:48:05 +0530

    addition

--- src/Com/calci/CalciImplementation.java
+++ src/Com/calci/CalciImplementation.java
@@ -4,8 +4,8 @@ public class CalciImplementation implements Calci{
 
 	@Override
 	public int add(int a, int b) {
-		// TODO Auto-generated method stub
-		return 0;
+		int c=a+b;
+		return c;
 	}
 
 	@Override
