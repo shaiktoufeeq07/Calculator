eb2f4b297ec08be5f3e9d91637fe799556aad1db
= <=>
2025-11-19 19:50:26 +0530

    multiplication method added

--- src/Com/calci/CalciImplementation.java
+++ src/Com/calci/CalciImplementation.java
@@ -16,8 +16,8 @@ public class CalciImplementation implements Calci{
 
 	@Override
 	public int mul(int a, int b) {
-		// TODO Auto-generated method stub
-		return 0;
+		int c=a*b;
+		return c;
 	}
 
 	@Override
